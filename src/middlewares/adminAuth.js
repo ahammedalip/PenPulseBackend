1c958cc8505e2daf673373ce53a2ec9702413a8b
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-09-10 12:27:32 +0530

    added admin auth middleware

---
+++ src/middlewares/adminAuth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.cookies.authToken; // Get token from the cookie
+
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided, authorization denied' });
+  }
+
+  try {
+    console.log('coming here to middleware=================' );
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach the user information to the request object
+    req.user = decoded;
+
+    next(); // Proceed to the next middleware or route handler
+  } catch (error) {
+    console.error('JWT verification failed:', error);
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+export default authenticateJWT;
