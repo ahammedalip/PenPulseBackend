d5d57a3aa335aa0bc35b832edff1be12b3b0d9f8
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-09-12 00:16:45 +0530

    admin token expires changed to 24 hour

--- src/controllers/adminControllers/adminAuthController.js
+++ src/controllers/adminControllers/adminAuthController.js
@@ -16,7 +16,7 @@ export const login = async (req, res) => {
         if (isAdmin && isAdmin.password != password) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
-        const token = jwt.sign({ id: isAdmin._id.toString(), role: 'Admin', username: isAdmin.username }, process.env.JWT_SECRET || '', { expiresIn: '1h' });
+        const token = jwt.sign({ id: isAdmin._id.toString(), role: 'Admin', username: isAdmin.username }, process.env.JWT_SECRET || '', { expiresIn: '24h' });
         res.status(200).json({ success: true, message: 'Admin logged in successfully', token: token });
     } catch (error) {
         console.log(`Error at admin login ${error}`)
